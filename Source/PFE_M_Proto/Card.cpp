4d5eb0606613ed0ffdf12ffabac6970097151b75
miriel mathis <miriel.mathis@gmail.com>
2024-10-16 14:05:23 +0200

    Addition of some BPC for camera movement for M02 + removing depricated cpp files

--- Source/PFE_M_Proto/Card.cpp
+++ Source/PFE_M_Proto/Card.cpp
@@ -1,12 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "CppCard.h"
+#include "Card.h"
 
-FCppCard::FCppCard()
+UCard::UCard()
 {
 }
 
-FCppCard::~FCppCard()
+UCard::~UCard()
 {
 }
---
+++ Source/PFE_M_Proto/Card.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Card.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS(Blueprintable)
+class PFE_M_PROTO_API UCard : public UObject
+{
+
+	GENERATED_BODY()
+	
+public:
+	UCard();
+	virtual ~UCard() override;
+};
